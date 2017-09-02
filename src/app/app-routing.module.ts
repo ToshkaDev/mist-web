d4ef2e17b6f5ad850d6b41206104a9dc6277f7b6
Luke Ulrich <lukeulrich@users.noreply.github.com>
2017-09-02 11:11:34 -0400

    mist setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,26 +1,23 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router';
 
-// import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GenomesComponent } from './genomes/genomes.component';
 
 const routes: Routes = [
+  { path: '', component: AppComponent, pathMatch: 'full' },
   { path: 'genomes', component: GenomesComponent },
 ];
 
 @NgModule({
-  bootstrap: [AppComponent],
-  declarations: [
-    AppComponent,
-    GenomesComponent,
+  exports: [
+    RouterModule,
   ],
   imports: [
-    BrowserModule,
-    // AppRoutingModule,
-    RouterModule.forRoot(routes),
+    RouterModule.forRoot(
+      routes,
+      { enableTracing: false },
+    ),
   ],
-  providers: [],
 })
-export class AppModule { }
+export class AppRoutingModule {}
---
+++ src/app/genomes/genomes.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mist-genomes',
+  styleUrls: [],
+  template: '<p>Genomes</p>',
+})
+export class GenomesComponent {
+}
