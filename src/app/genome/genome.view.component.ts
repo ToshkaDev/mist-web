5c7c0e49de0b15b10121367353955c1114cdb5fb
Vadim Gumerov <netuns@gmail.com>
2017-12-01 20:45:46 -0500

    Data on the genome page

--- src/app/genome/genome.view.component.ts
+++ src/app/genome/genome.view.component.ts
@@ -1,8 +1,23 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, AfterContentChecked } from '@angular/core';
 @Component({
   selector: 'mist-genome-view',
   templateUrl: './genome.view.component.pug',
 })
-export class GenomeViewComponent  {
-  @Input() genome: string[];
+export class GenomeViewComponent implements AfterContentChecked {
+  @Input() genome: any;
+  genomeTaxonomy: string[];
+
+  ngAfterContentChecked() {
+    if (this.genome) {
+        this.genomeTaxonomy = [
+          this.genome.superkingdom, 
+          this.genome.phylum, 
+          this.genome.class,
+          this.genome.order,
+          this.genome.family,
+          this.genome.genus,
+          this.genome.strain];
+    }
+
+  }
 }
