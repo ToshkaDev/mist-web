deff969be6e4963f0b9f9fdd1604e73e757ad80a
Vadim Gumerov <netuns@gmail.com>
2018-01-26 18:13:52 -0500

    Most specific taxonomy filter term is shown in Taxonomy column.

--- src/app/genomes/genomes.filter.ts
+++ src/app/genomes/genomes.filter.ts
@@ -1,14 +1,15 @@
 export class GenomesFilter {
-    readonly levels = 5;
+    readonly levels: number = 5;
+    private mostSpecificLevel: string;
 
-    //using two maps instead of just sending a number to resetFrom() for more robustness 
-    readonly taxonmoyToRang: Map<string,number> = new Map()
+    // using two maps instead of just sending a number to resetFrom() for more robustness 
+    readonly taxonmoyToRank: Map<string,number> = new Map()
     .set("phylum", 1)
     .set("clazz", 2)
     .set("orderr", 3)
     .set("family", 4)
     .set("genus", 5);
-    readonly rangToTaxonomy: Map<number,string> = new Map()
+    readonly rankToTaxonomy: Map<number,string> = new Map()
     .set(1,"phylum")
     .set(2,"clazz")
     .set(3,"orderr")
@@ -24,15 +25,32 @@ export class GenomesFilter {
         public assembly_level?: string
     ) {}
 
+    getMostSpecificLevel() {
+        if (this.genus) {
+            this.mostSpecificLevel = this.genus;
+        } else if (this.family) {
+            this.mostSpecificLevel = this.family;
+        } else if (this.orderr) {
+            this.mostSpecificLevel = this.orderr;
+        } else if (this.clazz) {
+            this.mostSpecificLevel = this.clazz;
+        } else if (this.phylum) {
+            this.mostSpecificLevel = this.phylum;
+        } else {
+            this.mostSpecificLevel = "defaultValue";
+        }
+        return this.mostSpecificLevel;
+    }
+
     reset() {
-        this.assembly_level=undefined;
+        this.assembly_level = undefined;
         this.resetFrom();
     }
 
     resetFrom(highestlevel: string = null) {
-        let startIndex = highestlevel ? this.taxonmoyToRang.get(highestlevel) : 0;
-        for (let level=++startIndex; level <= this.levels; level++) {
-            this[this.rangToTaxonomy.get(level)] = undefined;
+        let startIndex = highestlevel ? this.taxonmoyToRank.get(highestlevel) : 0;
+        for (let level = ++startIndex; level <= this.levels; level++) {
+            this[this.rankToTaxonomy.get(level)] = undefined;
         }
     }
 }
