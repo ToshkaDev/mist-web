99cca32a919d6098e82b74316ccb3203a84b41f3
Vadim Gumerov <netuns@gmail.com>
2018-02-15 16:04:06 -0500

    Some refactoring

--- src/app/genomes/genomes.filter.ts
+++ src/app/genomes/genomes.filter.ts
@@ -1,25 +1,28 @@
 export class GenomesFilter {
-    readonly levels: number = 5;
+    readonly levels: number = 6;
     private mostSpecificLevel: string;
 
     // using two maps instead of just sending a number to resetFrom() for more robustness 
     readonly taxonmoyToRank: Map<string,number> = new Map()
-    .set("phylum", 1)
-    .set("clazz", 2)
-    .set("orderr", 3)
-    .set("family", 4)
-    .set("genus", 5);
+    .set("superkingdom", 1)
+    .set("phylum", 2)
+    .set("clazz", 3)
+    .set("order", 4)
+    .set("family", 5)
+    .set("genus", 6);
     readonly rankToTaxonomy: Map<number,string> = new Map()
-    .set(1,"phylum")
-    .set(2,"clazz")
-    .set(3,"orderr")
-    .set(4,"family")
-    .set(5,"genus");
+    .set(1, "superkingdom")
+    .set(2,"phylum")
+    .set(3,"clazz")
+    .set(4,"order")
+    .set(5,"family")
+    .set(6,"genus");
     
-    constructor(  
+    constructor(
+        public superkingdom?: string, 
         public phylum?: string,
         public clazz?: string,
-        public orderr?: string,
+        public order?: string,
         public family?: string,
         public genus?: string,
         public assembly_level?: string
@@ -30,12 +33,14 @@ export class GenomesFilter {
             this.mostSpecificLevel = this.genus;
         } else if (this.family) {
             this.mostSpecificLevel = this.family;
-        } else if (this.orderr) {
-            this.mostSpecificLevel = this.orderr;
+        } else if (this.order) {
+            this.mostSpecificLevel = this.order;
         } else if (this.clazz) {
             this.mostSpecificLevel = this.clazz;
         } else if (this.phylum) {
             this.mostSpecificLevel = this.phylum;
+        } else if (this.superkingdom) {
+            this.mostSpecificLevel = this.superkingdom;
         } else {
             this.mostSpecificLevel = "defaultValue";
         }
