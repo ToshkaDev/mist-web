5d25e85ce3eb9fddbad8e5adf92f19c97914a796
Vadim Gumerov <netuns@gmail.com>
2018-02-19 11:20:33 -0500

    Introduction of genome.view.model and some little changes.

--- src/app/genome/genome.component.ts
+++ src/app/genome/genome.component.ts
@@ -1,23 +1,23 @@
-import {Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { Location } from '@angular/common';
 import { Observable } from 'rxjs/Observable';
 import { FetchGenome } from './genome.actions';
 import * as fromGenomes from './genome.selectors';
+import GenomeView from './genome.view.model';
 
 @Component({
     selector: 'mist-genome',
-    styleUrls: [],
     templateUrl: './genome.component.pug',
   })
 export class GenomeComponent implements OnInit {
-    errorMessage$: Observable<string>;
-    genome$: Observable<string>;
-    assemblyVersion: string;
-
+    private assemblyVersion: string;
+    private errorMessage$: Observable<string>;
+    private genome$: Observable<any>;
+    private genomeViewModel: GenomeView;
+    
     constructor(private store: Store<any>, private route: ActivatedRoute, private location: Location) {
-        console.log("From GenomeComponent");
     }
 
     ngOnInit() {
@@ -25,6 +25,8 @@ export class GenomeComponent implements OnInit {
         this.genome$ = this.store.select(fromGenomes.getFetchResult);       
         this.assemblyVersion = this.route.snapshot.paramMap.get('version');
         this.getGenome(this.assemblyVersion);
+        this.genome$.subscribe(result => 
+            result ? this.genomeViewModel = new GenomeView(result) : this.genomeViewModel = null);
     }
     
     getGenome(query: string) {
