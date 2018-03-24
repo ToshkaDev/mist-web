91decc82425cf6a667e81b995829c0e3e670a797
Vadim Gumerov <netuns@gmail.com>
2018-03-23 20:43:35 -0400

    Changes in negihbour gene visualization. Need to be revisited and refactored

--- src/app/gene/gene.view.component.ts
+++ src/app/gene/gene.view.component.ts
@@ -1,5 +1,4 @@
 import { Component, Input, AfterViewChecked, OnInit, ElementRef, ChangeDetectionStrategy } from '@angular/core';
-import * as DomainGfx from 'domain-gfx';
 import { Observable } from 'rxjs/Observable';
 import { D3Service, D3, Selection } from 'd3-ng2-service';
 
@@ -18,7 +17,7 @@ export class GeneViewComponent implements OnInit {
   private d3Svg: Selection<SVGSVGElement, any, null, undefined>; 
 
   readonly svgWidth = 1500;
-  readonly svgHeight = 200;
+  readonly svgHeight = 400;
   readonly fillColour = 'white';
   readonly borderColour = 'green';
 
@@ -37,64 +36,8 @@ export class GeneViewComponent implements OnInit {
   }
 
   ngOnInit() {
-
     if (this.parentNativeElement !== null)
       this.gene$.skip(1).take(1).subscribe(gene => gene && gene.length > 0 ? this.checkNeighbourGenesAndDraw(gene) : null)
-
-
-    // const parent = document.querySelector('.maincontainer');
-
-    // //testing domain-gfx library
-    // const data = {
-    //   length: 100,
-    //   regions: [
-    //     {
-    //       text: 'domain name',
-    //       start: 2,
-    //       end: 40,
-    //       aliStart: 5,
-    //       aliEnd: 30,
-    //       display: true,
-    //       startStyle: 'jagged',
-    //       endStyle: 'curved',
-    //       color: 'blue',
-    //       metadata: {
-    //         database: 'pfam',
-    //         description: 'text about the domain',
-    //         accession: 'PF00000',
-    //         identifier: 'domain X',
-    //       },
-    //     },
-    //   ],
-    //   markups: [
-    //     {
-    //       lineColour: '#0ff0f0',
-    //       colour: '#bb5b55',
-    //       display: true,
-    //       vAlign: 'top',
-    //       type: 'Pfam predicted active site',
-    //       start: 5,
-    //       headStyle: 'diamond',
-    //       metadata: {
-    //         database: 'pfam',
-    //         description: 'S Pfam predicted active site',
-    //       },
-    //     },
-    //   ],
-    //   motifs: [
-    //     {
-    //       colour: '#00a500',
-    //       metadata: true,
-    //       database: 'Phobius',
-    //       type: 'sig_p',
-    //       display: true,
-    //       end: 50,
-    //       start: 30
-    //     },
-    //   ],
-    // };
-     
-    // const dg = new DomainGfx({data, parent});
   }
 
   checkNeighbourGenesAndDraw(gene) {
@@ -102,7 +45,6 @@ export class GeneViewComponent implements OnInit {
   }
 
   drawGenes(gene: any, neighbGenes: any[]) {
-    
     let currentGeneColour = "#00cc66";
     let d3 = this.d3;
     let d3ParentElement: Selection<HTMLElement, any, null, undefined>;
@@ -141,25 +83,30 @@ export class GeneViewComponent implements OnInit {
     let geneStart;
     let geneStop;
     let genePath;
-    
-    let containerGroup = this.d3Svg.selectAll("g")
+  
+    let containerGroup = this.d3Svg.append("g").attr("transform", "translate(0,50)");
+
+    containerGroup.append("rect")
+    .attr("transform", "translate(0,0)")
+    .attr("fill-opacity", "0.00")
+    .attr("stroke", "gray")
+    .attr("stroke-width", 3)
+    .attr("rx", 7)
+    .attr("ry", 7)
+    .attr('width', 1500)
+    .attr('height', 200);
+
+    let geneCluster = containerGroup.selectAll("g")
     .data(neighbourGenes)
     .enter()
     .append("g")
     .attr("transform", function(d, i) {
-      return "translate(0,0)";
+      return "translate(0,35)";
     });
 
-    containerGroup.append("path")
+    geneCluster.append("path")
     .attr("d", function(gene, i) {
-      // console.log("gene.start " + gene.start)
-      // console.log("geneStop " + geneStop)
-      // console.log("geneScale(geneStart) " + geneScale(gene.start))
-      // console.log("geneScale(gene.stop) " + geneScale(gene.stop))
-
-
       isComplement = gene.strand === "-" ? true : false;
-      //console.log("gene.strand " + gene.strand)
       if (geneScale(gene.start) < geneScale(genomeNeighbStop)) {
         if (!isComplement) {
           genePath = [`M${geneScale(gene.start)}`, directGeneFigureTopY, 
@@ -191,37 +138,89 @@ export class GeneViewComponent implements OnInit {
     .attr("class", "gene-path");
 
 
-    containerGroup.append("text")
-      .attr('class', 'myc')
-      .attr("display", "none")
-      .attr("x", function(gene) {
-        return geneScale(gene.start);
-      }) 
-      .attr("y", function(gene) {
-        isComplement = gene.strand === "-" ? true : false;
-        if (!isComplement)
-          return directGeneFigureMiddlePointY;
-        return reverseGeneFigureMiddlePointY;
-      })
-      .attr("stroke", "black")
-      .attr("font-size", "12px")
-      .attr("font-family", "arial")
-      .text(function(gene) {
-        return gene.version;
-      });
 
-      containerGroup
+
+    geneCluster.append("rect")
+    .attr("display", "none")
+    .attr("class", "neighbGeneInfo")
+    .attr('width', 200)
+    .attr('height', 100)
+    .attr("fill-opacity", "1")
+    .attr("fill", "white")
+    .attr("stroke", "gray")
+    .attr("stroke-width", 1)
+    .attr("rx", 3)
+    .attr("ry", 3)
+    .attr("x", function(gene) {
+      return geneScale(gene.start);
+    })
+    .attr("y", function(gene) {
+      isComplement = gene.strand === "-" ? true : false;
+      if (!isComplement)
+        return directGeneFigureTopY-100;
+      return reverseGeneFigureTopY-100;
+    });
+
+
+
+    //Need to change this thing.
+    geneCluster.append("foreignObject")
+    .attr('class', 'geneText')
+    .attr("display", "none")
+    .attr("x", function(gene) {
+      return geneScale(gene.start);
+    }) 
+    .attr("y", function(gene) {
+      isComplement = gene.strand === "-" ? true : false;
+      if (!isComplement)
+        return directGeneFigureTopY-90;
+      return reverseGeneFigureTopY-90;
+    })
+    .attr("stroke", "black")
+    .attr("font-size", "12px")
+    .attr("font-family", "arial")
+    .html(function(gene) {
+      return `<div>${gene.stable_id}</div><div>${gene.version}</div><div><a routerLink href="/genes/${gene.stable_id}">${gene.product}</a></div>`;
+    });
+
+
+    // geneCluster.append("text")
+    //   .attr('class', 'geneText')
+    //   .attr("display", "none")
+    //   .attr("x", function(gene) {
+    //     return geneScale(gene.start);
+    //   }) 
+    //   .attr("y", function(gene) {
+    //     isComplement = gene.strand === "-" ? true : false;
+    //     if (!isComplement)
+    //       return directGeneFigureTopY;
+    //     return reverseGeneFigureTopY;
+    //   })
+    //   .attr("stroke", "black")
+    //   .attr("font-size", "12px")
+    //   .attr("font-family", "arial")
+    //   .text(function(gene) {
+    //     return gene.version;
+    //   });
+
+      geneCluster
       .on("mouseover", function (){
-        d3.select(this).select('text').attr("display", null).style("pointer-events", "none")
+        let element = d3.select(this);
+        element.select('.geneText').attr("display", null);
+        element.select('.neighbGeneInfo').attr("display", null);
+        element.raise();
       })
       .on("mouseout", function(){
-        d3.select(this).select('text').attr("display", "none")
+        d3.select(this).select('.geneText').attr("display", "none")
+        d3.select(this).select('.neighbGeneInfo').attr("display", "none")
       });
 
-      this.d3Svg.append("g")
-      .attr("transform", "translate(0, 55)")
+      //Add axis
+      containerGroup.append("g")
+      .attr("transform", "translate(0, 90)")
       .call(d3.axisBottom(geneScale).ticks(11));
   }
+
 
 }
 
