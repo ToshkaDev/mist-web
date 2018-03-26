283746f3712fa58cc8988fa090da7bb613566a7c
Vadim Gumerov <netuns@gmail.com>
2018-03-26 18:52:13 -0400

    Removed unnecessary imports

--- src/app/genes/genes-list.component.ts
+++ src/app/genes/genes-list.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, Input, Output, EventEmitter } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { DataSource } from '@angular/cdk/collections';
 
 @Component({
