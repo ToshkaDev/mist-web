0eb2a9a41421fd082a3ceaf4324ae35d2c299107
Vadim Gumerov <netuns@gmail.com>
2018-03-27 17:01:08 -0400

    Protein feature visualization in the table of genes.

--- src/app/core/common/fields.ts
+++ src/app/core/common/fields.ts
@@ -1,7 +1,7 @@
 const fieldMap = new Map<string, string>([
     ["GENOMES", "name,superkingdom,phylum,class,order,family,genus,genbank_version,version,assembly_level"],
     ["GENOME", ""],
-    ["GENES", "stable_id,version,names,locus,location,product&fields.Component=version,definition"],
+    ["GENES", "id,stable_id,version,names,locus,location,product&fields.Component=version,definition&fields.Aseq"],
     ["NEIGHBOUR_GENES",""],
     ["GENE","stable_id,aseq_id,version,start,stop,names,locus,old_locus,location,product,strand,length,cds_qualifiers,pseudo&fields.Component=version,definition&fields.Component.Genome=version&fields.Aseq"],
 ]);
--- src/app/genes/genes-list.component.ts
+++ src/app/genes/genes-list.component.ts
@@ -1,14 +1,31 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, ElementRef, AfterViewInit } from '@angular/core';
 import { DataSource } from '@angular/cdk/collections';
+import { Observable } from 'rxjs/Observable';
+import { D3Service } from 'd3-ng2-service';
+import DrawProteinFeature from '../core/common/drawSvg/draw-protein-feature';
 
 @Component({
-  changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'mist-genes-list',
   templateUrl: './genes-list.pug',
   styleUrls: ['./genes-list.scss']
 })
-export class GenesListComponent { 
+export class GenesListComponent implements AfterViewInit {
+  @Input() genes$: Observable<any>; 
   @Input() displayedColumns: String[];  
   @Input() genes: DataSource<any>;
   @Input() query: string;
+
+  private htmlElement: string = "div";
+    
+  constructor(private elementRef: ElementRef, private d3Service: D3Service) {
+  }
+
+  ngAfterViewInit() {
+      this.genes$.subscribe(result => {
+          for (let gene in result) {
+            let drawProteinFeature = new DrawProteinFeature(this.elementRef, this.d3Service);
+            drawProteinFeature.drawProteinFeature(`${this.htmlElement}.gene${result[gene]["id"]}`, [result[gene]["Aseq"]]);
+          }
+      });
+  }
 }
