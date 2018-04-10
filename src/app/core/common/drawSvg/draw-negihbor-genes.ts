9f1db1f0a5a8715ad53e84f2c52bf1432f642cf9
Vadim Gumerov <netuns@gmail.com>
2018-04-09 20:15:54 -0400

    refactored draw-negihbor-genes.ts

--- src/app/core/common/drawSvg/draw-negihbor-genes.ts
+++ src/app/core/common/drawSvg/draw-negihbor-genes.ts
@@ -7,12 +7,15 @@ export default class DrawNeighborGenes {
     private d3Element: Selection<HTMLBaseElement, any, null, undefined>;
     private d3Svg: Selection<SVGSVGElement, any, null, undefined>; 
 
-    static readonly svgWidth = 1500;
-    static readonly svgHeight = 500;
-    static readonly fillColour = 'white';
-    static readonly borderColour = 'green';
-    static readonly currentGeneColour = "#00cc66";
-  
+    //Figure
+    static svgWidth = 2000;
+    static svgHeight = 500;
+    static readonly clusterFrameHeight = DrawNeighborGenes.svgHeight*0.4;
+    static readonly yTranslationOfSvg = 95;
+    static readonly frameRectXRadius = 7;
+    static readonly frameRectYRadius = 7;
+
+    //Gene cluster
     static readonly directGeneFigureTopY = 20;
     static readonly geneFigureWidth = 30;
     static readonly directGeneFigureBottomY = DrawNeighborGenes.directGeneFigureTopY + DrawNeighborGenes.geneFigureWidth;
@@ -20,13 +23,31 @@ export default class DrawNeighborGenes {
     static readonly reverseGeneFigureTopY = DrawNeighborGenes.directGeneFigureBottomY+30;
     static readonly reverseGeneFigureBottomY = DrawNeighborGenes.reverseGeneFigureTopY + DrawNeighborGenes.geneFigureWidth;
     static readonly reverseGeneFigureMiddlePointY = DrawNeighborGenes.reverseGeneFigureTopY + DrawNeighborGenes.geneFigureWidth*0.5;
+    static readonly yTranslationOfGenes = 35;
     static readonly geneFigureArrowLen = 10;
+    static readonly fillColour = 'white';
+    static readonly borderColour = 'green';
+    static readonly currentGeneColour = "#00cc66";
+
+    //Gene info box
+    static readonly infoBoxWidth = 300;
+    static readonly infoBoxHeight = DrawNeighborGenes.svgHeight*0.3;
+    static readonly infoBoxRectXRadius = 3;
+    static readonly infoBoxRectYRadius = 3;
+    static readonly directGeneInfoBoxY = DrawNeighborGenes.directGeneFigureTopY-148;
+    static readonly reverseGeneInfoBoxY = DrawNeighborGenes.reverseGeneFigureTopY-150;
     static readonly textPositionFactorDirect = 0.81;
     static readonly textPositionFactorReverse = 0.87;
 
     constructor(element: ElementRef, d3Service: D3Service) {
         this.d3 = d3Service.getD3();
         this.parentNativeElement = element.nativeElement;
+    }
+
+    removeElement(htmlElement) {
+        let d3ParentElement = this.d3.select(this.parentNativeElement);
+        d3ParentElement.selectAll("g").remove();
+        d3ParentElement.selectAll("div").remove();
     }
 
     drawNeighborGenes(htmlElement, gene: any, neighbGenes: any[]) {
@@ -42,14 +63,13 @@ export default class DrawNeighborGenes {
         .domain([genomeNeighbStart, genomeNeighbStop])
         .range([0, DrawNeighborGenes.svgWidth]);
 
-
-        let containerGroup = this.d3Svg.append("g").attr("transform", "translate(0,95)");
+        let containerGroup = this.d3Svg.append("g").attr("transform", `translate(0,${DrawNeighborGenes.yTranslationOfSvg})`);
         let geneCluster = this.createFrameAndAppendGroupTags(containerGroup, [...neighbGenes, gene]);
         this.createGenePaths(geneCluster, gene, genomeNeighbStop, geneScale);
         this.createDescriptionBoxes(geneCluster, geneScale);
         //Add axis
         containerGroup.append("g").attr("class", "gene-axis")
-        .attr("transform", "translate(0, 90)")
+        .attr("transform", `translate(0,${DrawNeighborGenes.yTranslationOfSvg})`)
         .call(d3.axisBottom(geneScale).ticks(11));
         //create divs with gene descriptions
         let divs = this.addHtml([...neighbGenes, gene], d3ParentElement, geneScale);
@@ -76,8 +96,8 @@ export default class DrawNeighborGenes {
             let format = gene.strand === "-" ? "complement(coords)" : "(coords)";
             let geneCoordinates = format.replace("coords", gene.start + ".." + gene.stop);
             return `<div><a routerLink href="/genes/${gene.stable_id}">${gene.stable_id}</a></div>` + 
-            `<div>${gene.version}</div><div>${geneCoordinates}</div>` + 
-            `<div>${gene.product}<div/>`;
+                `<div>${gene.version}</div><div>${geneCoordinates}</div>` + 
+                `<div>${gene.product}<div/>`;
         })
         return divs;
     }
@@ -87,11 +107,11 @@ export default class DrawNeighborGenes {
         .attr("transform", "translate(0,0)")
         .attr("fill-opacity", "0.00")
         .attr("stroke", "gray")
-        .attr("stroke-width", 3)
-        .attr("rx", 7)
-        .attr("ry", 7)
-        .attr('width', 1500)
-        .attr('height', 200);
+        .attr("stroke-width", 2)
+        .attr("rx", DrawNeighborGenes.frameRectXRadius)
+        .attr("ry", DrawNeighborGenes.frameRectYRadius)
+        .attr('width', DrawNeighborGenes.svgWidth)
+        .attr('height', DrawNeighborGenes.clusterFrameHeight);
 
         return containerGroup.selectAll("g")
         .data(neighbourGenes)
@@ -101,7 +121,7 @@ export default class DrawNeighborGenes {
             return "gene"+d.id;
         })
         .attr("transform", function(d) {
-        return "translate(0,35)";
+            return `translate(0, ${DrawNeighborGenes.yTranslationOfGenes})`;
         });
     }
 
@@ -121,7 +141,7 @@ export default class DrawNeighborGenes {
                 `L${geneScale(gene.stop)}`, DrawNeighborGenes.directGeneFigureMiddlePointY, 
                 `L${geneScale(gene.stop)-DrawNeighborGenes.geneFigureArrowLen}`, DrawNeighborGenes.directGeneFigureBottomY, 
                 `L${geneScale(gene.start)}`, DrawNeighborGenes.directGeneFigureBottomY, 'Z'
-            ].join(" ")
+            ].join(" ");
         } else {
             genePath = [
                 `M${geneScale(gene.start)}`, DrawNeighborGenes.reverseGeneFigureMiddlePointY, 
@@ -129,7 +149,7 @@ export default class DrawNeighborGenes {
                 `L${geneScale(gene.stop)}`, DrawNeighborGenes.reverseGeneFigureTopY,
                 `L${geneScale(gene.stop)}`, DrawNeighborGenes.reverseGeneFigureBottomY, 
                 `L${geneScale(gene.start)+DrawNeighborGenes.geneFigureArrowLen}`, DrawNeighborGenes.reverseGeneFigureBottomY, 'Z'
-            ].join(" ")
+            ].join(" ");
         }
         prevEnd = gene.stop;
         return genePath;
@@ -140,7 +160,7 @@ export default class DrawNeighborGenes {
         return DrawNeighborGenes.fillColour;
         })
         .attr("stroke", function(gene) {
-        return DrawNeighborGenes.borderColour;
+            return DrawNeighborGenes.borderColour;
         })
         .attr("class", "gene-path");
     }
@@ -151,22 +171,22 @@ export default class DrawNeighborGenes {
         .attr("class", function(d) {
             return "gene"+d.id;
         })
-        .attr('width', 300)
-        .attr('height', 150)
+        .attr('width', DrawNeighborGenes.infoBoxWidth)
+        .attr('height', DrawNeighborGenes.infoBoxHeight)
         .attr("fill-opacity", "1")
         .attr("fill", "white")
         .attr("stroke", "gray")
         .attr("stroke-width", 1)
-        .attr("rx", 3)
-        .attr("ry", 3)
+        .attr("rx", DrawNeighborGenes.infoBoxRectXRadius)
+        .attr("ry", DrawNeighborGenes.infoBoxRectYRadius)
         .attr("x", function(gene) {
-        return geneScale(gene.start);
+            return geneScale(gene.start);
         })
         .attr("y", function(gene) {
         let isComplement = gene.strand === "-" ? true : false;
         if (!isComplement)
-            return DrawNeighborGenes.directGeneFigureTopY-148;
-        return DrawNeighborGenes.reverseGeneFigureTopY-150;
+            return DrawNeighborGenes.directGeneInfoBoxY;
+        return DrawNeighborGenes.reverseGeneInfoBoxY;
         });
     }
 
