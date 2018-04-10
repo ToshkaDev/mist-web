60790b35b94db9702f2cc078ad2ff2dd473305d2
Vadim Gumerov <netuns@gmail.com>
2018-04-09 20:17:15 -0400

    The size of genes cluster made responsive to the screen width.

--- src/app/gene/neighbor-genes/neighbor-genes.view.component.ts
+++ src/app/gene/neighbor-genes/neighbor-genes.view.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, ElementRef } from '@angular/core';
+import { Component, OnInit, Input, ElementRef, HostListener, Renderer2 } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { Location } from '@angular/common';
 import { Observable } from 'rxjs/Observable';
@@ -14,17 +14,54 @@ export class NeighborGenesView implements OnInit {
     @Input() private gene$: Observable<any>;
     @Input() private neighbourGenes$: Observable<any>;
     private htmlElement: string = "svg.geneCluster";
+    private thisGene: any;
+    private theseNeighbourGenes: any[];
+    private drawNeighborGenesObject: DrawNeighborGenes;
+    static readonly minSvgWidth = 650;
+    static readonly svgWidthToScreenWidthFactor = 0.94;
     
-    constructor(private elementRef: ElementRef, private d3Service: D3Service) {
+    constructor(private elementRef: ElementRef, private d3Service: D3Service, private renderer: Renderer2) {
     }
 
     ngOnInit() {
-        let drawNeighborGenesObject = new DrawNeighborGenes(this.elementRef, this.d3Service);
-        this.gene$.skip(1).take(1).subscribe(gene => gene && gene.length > 0 ? this.checkNeighbourGenesAndDraw(gene, drawNeighborGenesObject) : null)
+        let geneClusterSvgWidth = (window.innerWidth > 0) 
+            ? window.innerWidth*NeighborGenesView.svgWidthToScreenWidthFactor
+            : screen.width*NeighborGenesView.svgWidthToScreenWidthFactor;
+        geneClusterSvgWidth = geneClusterSvgWidth > NeighborGenesView.minSvgWidth 
+            ? geneClusterSvgWidth 
+            : NeighborGenesView.minSvgWidth;
+
+        DrawNeighborGenes.svgWidth = geneClusterSvgWidth;
+        this.drawNeighborGenesObject = new DrawNeighborGenes(this.elementRef, this.d3Service);
+        this.gene$.skip(1).take(1).subscribe(gene => {
+            if (gene && gene.length > 0) {
+                this.thisGene = gene;
+                this.checkNeighbourGenesAndDraw(gene, this.drawNeighborGenesObject);
+            }
+        });
     }
-    
+
+    @HostListener('window:resize', ['$event'])
+    onWindowResize(ev) {
+        console.log('resized', ev);
+        let geneClusterSvgWidth = (window.innerWidth > 0) 
+            ? window.innerWidth*NeighborGenesView.svgWidthToScreenWidthFactor
+            : screen.width*NeighborGenesView.svgWidthToScreenWidthFactor;
+        geneClusterSvgWidth = geneClusterSvgWidth > NeighborGenesView.minSvgWidth 
+            ? geneClusterSvgWidth 
+            : NeighborGenesView.minSvgWidth;
+
+        this.drawNeighborGenesObject.removeElement(this.htmlElement);
+        DrawNeighborGenes.svgWidth = geneClusterSvgWidth;
+        this.drawNeighborGenesObject.drawNeighborGenes(this.htmlElement, this.thisGene, this.theseNeighbourGenes);
+    }
+
     checkNeighbourGenesAndDraw(gene, drawNeighborGenesObject) {
-        this.neighbourGenes$.skip(1).take(1).subscribe(neighbGenes => 
-            neighbGenes && neighbGenes.length > 0 ? drawNeighborGenesObject.drawNeighborGenes(this.htmlElement, gene, neighbGenes) : null)
+        this.neighbourGenes$.skip(1).take(1).subscribe(neighbGenes => {
+            if (neighbGenes && neighbGenes.length > 0) {
+                this.theseNeighbourGenes = neighbGenes;
+                drawNeighborGenesObject.drawNeighborGenes(this.htmlElement, gene, neighbGenes);
+            }
+        });
     }
 }
