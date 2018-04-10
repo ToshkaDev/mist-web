b0f15efac3c55e55362fb9425de4a81aa6155fda
Vadim Gumerov <netuns@gmail.com>
2018-04-10 19:10:50 -0400

    Dinamic adding of genes and genomes components.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ComponentFactoryResolver, Type, OnInit } from '@angular/core';
+import { MistDirective } from './mist.directive';
+import { GenomesComponent } from '../genomes/genomes.component';
+import { GenesComponent } from '../genes/genes.component';
 
 @Component({
   selector: 'mist-home',
+  styleUrls: ['./home.scss'],
   templateUrl: './home.component.pug',
 })
 export class HomeComponent {
+  @ViewChild(MistDirective) adHost: MistDirective;
+  selected = 'Genomes';
+
+
+  constructor(private componentFactoryResolver: ComponentFactoryResolver) {}
+
+
+  ngOnInit() {
+    let componentFactory = this.componentFactoryResolver.resolveComponentFactory(GenomesComponent);
+    this.adHost.viewContainerRef.createComponent(componentFactory);
+  }
+
+  entityChanged(entity: any) {
+    let componentToCreate: any = entity.value == "Genomes" ? GenomesComponent : GenesComponent;
+    let componentFactory = this.componentFactoryResolver.resolveComponentFactory(componentToCreate);
+    let viewContainerRef = this.adHost.viewContainerRef;
+    viewContainerRef.clear();
+    viewContainerRef.createComponent(componentFactory);
+  }
 
 }
