9ae08d1cd43e011dd84e501512fba6e10ff312de
Vadim Gumerov <netuns@gmail.com>
2018-04-24 15:47:41 -0400

    Dynamic resizing of protein features in concordance with window size.

--- src/app/genes/genes-list.component.ts
+++ src/app/genes/genes-list.component.ts
@@ -15,8 +15,8 @@ export class GenesListComponent implements OnInit {
   @Input() displayedColumns: String[];  
   @Input() genes: DataSource<any>;
   @Input() query: string;
-  static readonly minSvgWidth = 200;
-  static readonly svgWidthToScreenWidthFactor = 0.30;
+  static readonly minSvgWidth = 100;
+  static readonly svgWidthToScreenWidthFactor = 0.33;
 
   private geneToAseq = new Map<string, any>();
   private geneIsDrawn = new Map<string, boolean>();
@@ -28,26 +28,28 @@ export class GenesListComponent implements OnInit {
   }
  
   ngOnInit() {
-      this.genes$.subscribe(result => {
-        this.geneToAseq.clear();
-        this.geneIsDrawn.clear();
-        for (let gene of result) {
-          if (gene.Aseq) {
-            this.geneToAseq.set(gene.id, gene.Aseq);
-            this.geneIsDrawn.set(gene.id, false);
-          }
+    this.genes$.subscribe(result => {
+      this.geneToAseq.clear();
+      this.geneIsDrawn.clear();
+      this.geneToProteinObject.clear();
+      for (let gene of result) {
+        if (gene.Aseq) {
+          this.geneToAseq.set(gene.id, gene.Aseq);
+          this.geneIsDrawn.set(gene.id, false);
         }
-      });
+      }
+    });
   }
 
   drawProteinFeature(geneId: string) {
-    console.log("trying to draw")
+    let svgWidth = this.getSvgWidth();
     let aseqData = this.geneToAseq.get(geneId);
     // !this.geneIsDrawn.get(geneId) is an additional guard preventing repeated rendering
     if (aseqData && !this.geneIsDrawn.get(geneId)) {
       let drawProteinFeature = new DrawProteinFeature(this.elementRef, this.d3Service);
-      this.geneToProteinObject.set(geneId, drawProteinFeature);
+      drawProteinFeature.setSvgSize(svgWidth);
       drawProteinFeature.drawProteinFeature(`${this.htmlElement}.gene${geneId}`, [aseqData]);
+      this.geneToProteinObject.set(geneId, drawProteinFeature);
     }
     // Even if something went wrong rendering shouldn't be repeated
     this.geneIsDrawn.set(geneId, true);
@@ -55,19 +57,20 @@ export class GenesListComponent implements OnInit {
 
   @HostListener('window:resize', ['$event'])
   onWindowResize(ev) {
-    let svgWidth = (window.innerWidth > 0) 
-      ? window.innerWidth*GenesListComponent.svgWidthToScreenWidthFactor
-      : screen.width*GenesListComponent.svgWidthToScreenWidthFactor;
-
-      svgWidth = svgWidth > GenesListComponent.minSvgWidth 
-      ? svgWidth 
-      : GenesListComponent.minSvgWidth;
-
+    let svgWidth = this.getSvgWidth();
     this.geneToProteinObject.forEach((proteinObject, geneId) => {
       proteinObject.removeElement(`${this.htmlElement}.gene${geneId}`);
       proteinObject.setSvgSize(svgWidth);
       proteinObject.drawProteinFeature(`${this.htmlElement}.gene${geneId}`, [this.geneToAseq.get(geneId)]);
-
     })
+  }
+
+  private getSvgWidth(): number {
+    let svgWidth = (window.innerWidth > 0) 
+      ? window.innerWidth*GenesListComponent.svgWidthToScreenWidthFactor
+      : screen.width*GenesListComponent.svgWidthToScreenWidthFactor;
+    return svgWidth > GenesListComponent.minSvgWidth 
+      ? svgWidth 
+      : GenesListComponent.minSvgWidth;
   }
 }
