28fa8aa660fb0d4300aac197fa7bd0160c37c9b1
Vadim Gumerov <netuns@gmail.com>
2018-07-02 18:58:39 -0400

    Started refactoring of components.

---
+++ src/app/genes/genes.main.ts
@@ -0,0 +1,91 @@
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import 'rxjs/add/operator/take';
+import { Observable } from 'rxjs/Observable';
+import { DataSource } from '@angular/cdk/collections';
+import { Search, FirstPage, LastPage, PrevPage, NextPage } from './genes.actions';
+import GenesFilter from './genes.filter';
+import * as fromGenes from './genes.selectors';
+import MistdDatasource from '../core/common/mist.datasource';
+import { Navigation }  from '../core/common/navigation';
+
+export class GenesComponent implements OnInit {
+    readonly defaultCount: number = 1;
+    readonly defaultCurrentPage: number = 1;
+    readonly defaultPerPage: number = 30;
+    readonly defaultTotalPages: number = 1;
+    readonly minQueryLenght = 1;
+    readonly columns: string[] = ["Select", "Mist Id", "Protein Id", "Domain Structure", "Locus", "Description", "Location"];
+    readonly pageSizeOptions = [5, 10, 30, 100];
+    private genes$: Observable<any[]>;
+    private query$: Observable<string>;
+    private links$: Observable<any>;
+    count: number;
+    totalPages: number;
+    perPage: number = 5;
+    currentPage: number;
+    displayedColumns: String[];
+    genesFilter: GenesFilter = new GenesFilter(); 
+    dataSource = new MistdDatasource(this.store, fromGenes.getSearchResults);
+  
+    constructor(private store: Store<any>) {}
+  
+    ngOnInit() {
+      this.query$ = this.store.select(fromGenes.getSearchQuery);
+      this.genes$ = this.store.select(fromGenes.getSearchResults);
+      this.links$ = this.store.select(fromGenes.getPageLinks);
+      this.store.select(fromGenes.getPageInfo).subscribe(
+        pageInfo => {
+          pageInfo.count ? this.count = pageInfo.count : this.count = this.defaultCount;
+          pageInfo.currentPage ? this.currentPage = pageInfo.currentPage : this.currentPage = this.defaultCurrentPage;
+          pageInfo.totalPages ? this.totalPages = pageInfo.totalPages : this.totalPages = this.defaultTotalPages;
+          pageInfo.perPage ? this.perPage = pageInfo.perPage: this.perPage = this.defaultPerPage;
+        }
+      );
+      this.genes$.subscribe(results => results.length > 0 ? this.displayedColumns = this.columns : this.displayedColumns = null);  
+    }
+  
+    pageApply($event) {
+      let eventPageIndex = ++$event.pageIndex;
+      let filter: GenesFilter;
+      //If actual filtering will be set up uncomment the line below
+      //this.query$.subscribe(searchterm => filter=this.checkQuery()).unsubscribe();
+      if ($event.pageSize !== this.perPage) {
+        this.perPage = $event.pageSize;
+        this.query$.subscribe(searchterm => this.search(searchterm)).unsubscribe();
+      } else if (eventPageIndex > this.currentPage) {
+          this.links$.subscribe(link => this.store.dispatch(new NextPage(new Navigation(link.next, filter)))).unsubscribe();
+      } else if (eventPageIndex < this.currentPage) {
+          this.links$.subscribe(link => this.store.dispatch(new PrevPage(new Navigation(link.prev, filter)))).unsubscribe();
+      } else if (eventPageIndex === 1) {
+          this.links$.subscribe(link => this.store.dispatch(new FirstPage(new Navigation(link.first, filter)))).unsubscribe();
+      } else if (eventPageIndex === this.totalPages) {
+          this.links$.subscribe(link => this.store.dispatch(new LastPage(new Navigation(link.last, filter)))).unsubscribe();
+      }
+    }
+  
+    search(query: string) {
+      this.store.dispatch(new Search({
+        search: query, 
+        perPage: this.perPage, 
+        pageIndex: this.defaultCurrentPage, 
+        filter: {}
+      }));
+    }
+  
+    checkQuery() {
+      let filter: GenesFilter;
+      this.query$.subscribe(searchterm => {
+        if (searchterm && searchterm.length >= this.minQueryLenght) {
+          filter = Object.assign(new GenesFilter(), this.genesFilter)
+        } else {
+            filter = Object.assign(new GenesFilter(), this.genesFilter.reset());
+        }
+      }).unsubscribe();
+      return filter;
+    }
+  
+    filter() {}
+  
+    reset() {}
+  }
---
+++ src/app/genomes/genomes.main.ts
@@ -0,0 +1,76 @@
+import { OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import 'rxjs/add/operator/take';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/from';
+import { FirstPage, LastPage, PrevPage, NextPage } from './genomes.actions';
+import * as fromGenomes from './genomes.selectors';
+import GenomesFilter from './genomes.filter';
+import { Navigation }  from '../core/common/navigation';
+import MistdDatasource from '../core/common/mist.datasource';
+
+export abstract class GenomesMain implements OnInit {
+  readonly defaultCount: number = 1;
+  readonly defaultCurrentPage: number = 1;
+  readonly defaultPerPage: number = 30;
+  readonly defaultTotalPages: number = 1;
+  readonly minQueryLenght = 1;
+  readonly columns = ['Select', 'Genome', 'Superkingdom', 'Taxonomy', 'Genbank Version', 'Assembly level'];
+  readonly pageSizeOptions = [5, 10, 30, 100];
+  protected query$: Observable<string>;
+  protected genomes$: Observable<any[]>;
+  protected links$: Observable<any>;
+  protected count: number;
+  protected totalPages: number;
+  protected perPage: number;
+  protected currentPage: number;
+  protected dataSource: MistdDatasource = new MistdDatasource(this.store, fromGenomes.getSearchResults);
+  protected defaultSelection: string = "defaultValue";
+  protected selected: string = this.defaultSelection;  
+  protected displayedColumns: String[];
+
+  constructor(private store: Store<any>) {
+  }
+
+  ngOnInit() {
+    this.query$ = this.store.select(fromGenomes.getSearchQuery);
+    this.genomes$ = this.store.select(fromGenomes.getSearchResults);
+    this.links$ = this.store.select(fromGenomes.getPageLinks);
+    this.store.select(fromGenomes.getPageInfo).subscribe(
+      pageInfo => {
+        pageInfo.count ? this.count = pageInfo.count : this.count = this.defaultCount;
+        pageInfo.currentPage ? this.currentPage = pageInfo.currentPage : this.currentPage = this.defaultCurrentPage;
+        pageInfo.totalPages ? this.totalPages = pageInfo.totalPages : this.totalPages = this.defaultTotalPages;
+        pageInfo.perPage ? this.perPage = pageInfo.perPage: this.perPage = this.defaultPerPage;
+      }
+    );
+    this.genomes$.subscribe(results => results.length > 0 ? this.displayedColumns = this.columns : this.displayedColumns = null);
+  }
+
+  pageApply($event) {
+    let eventPageIndex = ++$event.pageIndex;
+    let filter: GenomesFilter = this.initialyzeFilter();
+    if ($event.pageSize !== this.perPage) {
+      this.perPage = $event.pageSize;
+      this.sendQuery();
+    } else if (eventPageIndex > this.currentPage) {
+        this.links$.subscribe(link => this.store.dispatch(new NextPage(new Navigation(link.next, filter)))).unsubscribe();
+    } else if (eventPageIndex < this.currentPage) {
+        this.links$.subscribe(link => this.store.dispatch(new PrevPage(new Navigation(link.prev, filter)))).unsubscribe();
+    } else if (eventPageIndex === 1) {
+        this.links$.subscribe(link => this.store.dispatch(new FirstPage(new Navigation(link.first, filter)))).unsubscribe();
+    } else if (eventPageIndex === this.totalPages) {
+        this.links$.subscribe(link => this.store.dispatch(new LastPage(new Navigation(link.last, filter)))).unsubscribe();
+    }
+  }
+
+  protected getStore() {
+      return this.store;
+  }
+
+  abstract initialyzeFilter(): GenomesFilter;
+
+  //default sendQuery
+  abstract sendQuery(): void;
+
+}
