a74ffbdd66f2f6bca9135ebb281adb11ad6f0376
Luke Ulrich <lukeulrich@users.noreply.github.com>
2019-05-16 17:26:53 -0400

    add chemotaxis details to gene page

--- src/app/core/common/stp-utils.ts
+++ src/app/core/common/stp-utils.ts
@@ -31,6 +31,14 @@ export const PrimaryRankMap = {
   [PrimaryRank.tcp]: 'TCP',
 };
 
+export const PrimaryRankNameMap = {
+  [PrimaryRank.chemotaxis]: 'Chemotaxis',
+  [PrimaryRank.ecf]: 'Extracytoplasmic Function Sigma Factor',
+  [PrimaryRank.ocp]: 'One-component protein',
+  [PrimaryRank.other]: 'Other',
+  [PrimaryRank.tcp]: 'Two-component protein',
+};
+
 export const SecondaryRankMap = {
   [SecondaryRank.chea]: 'CheA',
   [SecondaryRank.cheb]: 'CheB',
@@ -48,6 +56,23 @@ export const SecondaryRankMap = {
   [SecondaryRank.rr]: 'RR',
 };
 
+export const SecondaryRankNameMap = {
+  [SecondaryRank.chea]: 'CheA',
+  [SecondaryRank.cheb]: 'CheB',
+  [SecondaryRank.checx]: 'CheCX',
+  [SecondaryRank.ched]: 'CheD',
+  [SecondaryRank.cher]: 'CheR',
+  [SecondaryRank.chev]: 'CheV',
+  [SecondaryRank.chew]: 'CheW',
+  [SecondaryRank.chez]: 'CheZ',
+  [SecondaryRank.hhk]: 'Hybrid histidine kinase',
+  [SecondaryRank.hk]: 'Histidine kinase',
+  [SecondaryRank.hrr]: 'Hybrid response regulator',
+  [SecondaryRank.mcp]: 'Methyl-accepting chemotaxis protein',
+  [SecondaryRank.other]: 'Other',
+  [SecondaryRank.rr]: 'Response regulator',
+};
+
 export const capitalizeRanks = (ranks: string[]): string[] => {
   const numRanks = ranks.length;
   if (!numRanks) {
@@ -58,6 +83,20 @@ export const capitalizeRanks = (ranks: string[]): string[] => {
   result[0] = PrimaryRankMap[ranks[0]] || ranks[0];
   if (numRanks > 1) {
     result[1] = SecondaryRankMap[ranks[1]] || ranks[1];
+  }
+  return result;
+};
+
+export const unabbreviateRanks = (ranks: string[]): string[] => {
+  const numRanks = ranks.length;
+  if (!numRanks) {
+    return [];
+  }
+
+  const result = [...ranks];
+  result[0] = PrimaryRankNameMap[ranks[0]] || ranks[0];
+  if (numRanks > 1) {
+    result[1] = SecondaryRankNameMap[ranks[1]] || ranks[1];
   }
   return result;
 };
--- src/app/pipes/gene-ranks.pipe.ts
+++ src/app/pipes/gene-ranks.pipe.ts
@@ -1,15 +1,19 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
-import { capitalizeRanks } from '../core/common/stp-utils';
+import { capitalizeRanks, unabbreviateRanks } from '../core/common/stp-utils';
 
 @Pipe({
   name: 'geneRanks',
   pure: true,
 })
 export class GeneRanksPipe implements PipeTransform {
-  transform(ranks: string[]): string {
+  transform(ranks: string[], full: boolean): string {
     if (!ranks || ranks.length < 1) {
       return null;
+    }
+
+    if (full) {
+      return unabbreviateRanks(ranks).join(', ');
     }
 
     return this.getCapitalizedRanks(ranks).join(', ');
