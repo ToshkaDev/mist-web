5de56921a131dbea37893d0b1eedc97ef668ae55
Luke Ulrich <lukeulrich@users.noreply.github.com>
2020-03-03 18:14:54 -0500

    show chemotaxis total if no subtypes (e.g. CheW)

--- src/app/core/components/chemotaxis-count-links/chemotaxis-count-links.component.ts
+++ src/app/core/components/chemotaxis-count-links/chemotaxis-count-links.component.ts
@@ -1,4 +1,5 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
+
 import { PrimaryRank, SecondaryRank } from '../../common/stp-utils';
 
 @Component({
@@ -7,21 +8,32 @@ import { PrimaryRank, SecondaryRank } from '../../common/stp-utils';
   styleUrls: ['./chemotaxis-count-links.scss'],
   templateUrl: './chemotaxis-count-links.pug',
 })
-export class ChemotaxisCountLinksComponent {
+export class ChemotaxisCountLinksComponent implements OnInit {
   @Input() counts: any;
   @Input() secondaryRank: SecondaryRank;
   @Input() componentId: number;
 
+  hasSubTypes = true;
+
+  ngOnInit() {
+    this.hasSubTypes = this.counts && this.counts[this.secondaryRank] && Object.keys(this.counts[this.secondaryRank]).length > 1;
+  }
+
   queryParams(secondaryRank: string, type: string) {
     const { componentId } = this;
-    // for some reason 24H,28H and MAC2 need differently ordered query parameters
-    let ranks = type === '24H' || type === '28H' || type === 'MAC2' 
-      ? `${type},${PrimaryRank.chemotaxis},${secondaryRank}`
-      : `${PrimaryRank.chemotaxis},${secondaryRank},${type}`;
+    let ranks = `${PrimaryRank.chemotaxis},${secondaryRank}`;
+    if (type) {
+      // for some reason 24H,28H and MAC2 need differently ordered query parameters
+      if (type === '24H' || type === '28H' || type === 'MAC2') {
+        ranks = `${type},${ranks}`;
+      } else {
+        ranks = `${ranks},${type}`;
+      }
+    }
 
     return {
       ...(componentId && {componentId}),
-      ranks: ranks,
+      ranks,
     };
   }
 }
